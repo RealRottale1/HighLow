64326b776ba5d18c6f9f6d18312082507a2e1d0a
RealRottale1 <camarkham2@gmail.com>
2025-02-14 12:29:24 -0500

    gftgrr

--- game.js
+++ game.js
@@ -1,44 +1,25 @@
-let randomNumber = Math.floor(Math.random() * 10) + 1;
-let userGuess;
+let randomNumber = Math.ceil(Math.random() * 10);
 let attempts = 0;
-let terminated = false;
-
-document.getElementById("button").addEventListener("click", function() {
-    if (terminated) {
-        return;
-    }
-    userGuess = document.getElementById("input").value;
-    const numberGuess = Number(userGuess);
-    if (isNaN(numberGuess)) {
-        document.getElementById("p").textContent = "Invalid Input!";
+let guess = window.prompt('Enter a guess for the number between 1 and 999');
+guess = Number(guess);
+ 
+ 
+while (guess != randomNumber || guess != 999){
+    if (isNaN(guess)) {
+        guess = window.prompt('Invalid Input');
     } else {
-        if (numberGuess == 999) {
-            document.getElementById("p").textContent = "Game Terminated!";
-            terminated = true;
-        } else {
-            attempts += 1;
-            if (numberGuess == randomNumber) {
-                document.getElementById("p").textContent = `You Guessed The Correct Number (${randomNumber}) In ${attempts} attempt${(attempts === 1 ? "" : "s")}!`;
-                terminated = true;
-            } else if ( numberGuess > randomNumber) {
-                document.getElementById("p").textContent = "Too High!";
-            } else {
-                document.getElementById("p").textContent = "Too Low!";
-            }
+        attempts += 1;
+        if(guess == 999) {
+            window.alert(`You entered 999 breaking out of the game`);
+            break;
+        } else if (guess == randomNumber) {
+            window.alert("You won in"+attempts+" attempts!");
+            break;
+        } else if (guess > randomNumber) {
+            guess = window.prompt('This number is to high, try again');
+        }else{
+            guess = window.prompt('This number is to low, try again');
         }
     }
-
-});
-
-// Prompt user for a number between 1 and 10, or type 999 to exit.
+}
  
-// Create while loop that continues for as long as the user guesses are incorrect.
-// Loop should count the number of guesses.
-// Loop should exit if user enters 999 and not display "Too high!"
-// If the guess is too low, the user should be promped "Too low!" and be able to guess again.
-// If the guess is too high, the user should be promped 'Too high!" and be able to guess again.
-// If anything else is entered, the user should be prompted that their input is invalid and 
-// to guess again.
-
-// If the guess is correct, the user should be alerted of their win and 
-// be told the number of guesses they made.
